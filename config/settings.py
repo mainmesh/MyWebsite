d743ef7ad3ad0342aae0c3189a9eaf0dce6786e3
mainmesh <meshackmbithi01@gmail.com>
2026-09-06 10:36:44 +0300

    Switch to non-manifest static storage to fix Vercel 500

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
