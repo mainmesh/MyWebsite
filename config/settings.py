b9aaa0c9f69c7a50cdf19e4bd3daf0eea9dda1f8
mainmesh <meshackmbithi01@gmail.com>
2026-09-06 08:14:22 +0300

    Switch to non-manifest staticfiles storage for Vercel

--- config/settings.py
+++ config/settings.py
@@ -79,9 +79,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+except Exception:
+    pass
